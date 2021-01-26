ede93ac049710ef781691b591e35759d86cb6c4f
Kevin Cartwright <kevincart@gmail.com>
2021-01-26 15:31:19 +1300

    trying to setup

--- EFCoreApp/Startup.cs
+++ EFCoreApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +33,11 @@ namespace EFCoreApp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "EFCoreApp", Version = "v1" });
             });
+            
+            var connectionString = Configuration["DbContextSettings:ConnectionString"];
+            services.AddDbContext<ArticleContext>(
+                opts => opts.UseNpgsql(connectionString)
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
